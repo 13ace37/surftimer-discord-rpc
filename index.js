cb5979ca16b8e7cebd6b6ecd809bd5cef4cdb8fe
Riccardo H <ace_graphics@outlook.de>
2019-10-11 04:40:02 +0200

    added rpc only for horizon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http'); //http module
 const port = 5555; //defines server port    
 const host = '127.0.0.1'; //defines server host
 
-const rpc = require('discord-rich-presence')("559388357654872064"); //discord rich-presence module
+const rpc = require('discord-rich-presence')("632010579464290304"); //discord rich-presence module
 
 const server = http.createServer((req, res) => { //sorry but im to fcking lazy to comment all this ._.
     if (req.method == 'POST') {
@@ -38,8 +38,7 @@ const server = http.createServer((req, res) => { //sorry but im to fcking lazy t
 server.listen(port, host);
 console.log(`Server is listening to ${host}:${port}`);
 
-var locktime = false;
-var idle;
+const date = new Date();
 
 function setRP(data) {
     if (!data.map) {
@@ -50,6 +49,7 @@ function setRP(data) {
                 state: 'N/A',
                 largeImageKey: 'icon',
                 smallImageText: data.player.name,
+                startTimestamp: date
             });
         } catch (e) {
             throw (e);
@@ -57,35 +57,24 @@ function setRP(data) {
     } else {
         let surfing = 'surfing';
         if (data.provider.steamid != data.player.steamid) surfing = 'spectating';
-        var time = "--:--";
-        var rank = "N/A";
-        if (!data.previously || data.previously == undefined) idle = setTimeout(function() {locktime = true;}, 25000);
-        else {
-            clearTimeout(idle);
-            locktime = false;
-        } 
-        if (locktime) surfing = "idling";
+        var maps = "N/A";
+        var mapRank = "N/A";
+        var serverRank = "N/A";
 
-        if (data.player.match_stats.kills > 0) {
-            let ms = data.player.match_stats.kills * 1000;
-            let round = ms > 0 ? Math.floor : Math.ceil;
-            let minutes = fixTime(round(ms / 60000) % 60);
-            let seconds = fixTime(round(ms / 1000) % 60);
-            let milli = round(ms) % 1000;
-            if (milli.length > '2') milli = fixTime(milli.slice(0, -1));
-            else milli = fixTime(milli);
-            time = `${minutes}:${seconds}`;
-        } else time = "--:--";
+        if (data.player.match_stats.deaths > 0) mapRank = data.player.match_stats.deaths;
+        if (data.player.match_stats.kills > 0) maps = data.player.match_stats.kills;
+        if (data.player.match_stats.score < 0 && data.player.match_stats.score != -99999) serverRank = String(data.player.match_stats.score).slice(1);
         var map = data.map.name.split('/')[2] || data.map.name;
         if (map.startsWith('surf_')) {
             if (Number(String(data.player.match_stats.score).slice(1)) < 99999) rank = String(data.player.match_stats.score).slice(1);
             try {
                 confirm({
                     details: `${surfing} on ${map}`,
-                    state: `Timer: ${time} | Rank: ${rank}`,
+                    state: `Maps: ${maps} | Ranks: ${mapRank} / ${serverRank}`,
                     largeImageKey: 'icon',
                     largeImageText: data.player.clan + " " + data.player.name,
                     smallImageText: data.player.name,
+                    startTimestamp: date
                 });
             } catch (e) {
                 throw (e);
@@ -103,6 +92,7 @@ const fix = {
     largeImageKey: '',
     largeImageText: "",
     smallImageText: "",
+    startTimestamp: date
 };
 
 function confirm(rp) {
@@ -112,9 +102,4 @@ function confirm(rp) {
     } catch (e) {
         throw (e);
     }
-}
-
-function fixTime(number) {
-    if (number < 10) number = '0' + number;
-    return number;
 }
